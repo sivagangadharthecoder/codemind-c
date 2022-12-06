cdc9fdc3e345c8dafe3c55f852f1b7a22ab702b3
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2022-12-06 09:56:59 +0530

    //INCHES TO CENTIMETRES (FIRST LOGIC BEGINNER – BASIC PROGRAMME) #include<stdio.h> int main() {     int in;     scanf("%d",&in);     float cm;     cm= in*2.54;     printf("%0.2f",cm); }

---
+++ inches_to_centimeters.c
@@ -0,0 +1,10 @@
+//INCHES TO CENTIMETRES (FIRST LOGIC BEGINNER – BASIC PROGRAMME)
+#include<stdio.h>
+int main()
+{
+    int in;
+    scanf("%d",&in);
+    float cm;
+    cm= in*2.54;
+    printf("%0.2f",cm);
+}
