979d01a21161904df1b33e60633cfffece06c007
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2022-12-06 10:44:21 +0530

    //AVERAGE OF TWO NUMBERS (FIRST LOGIC BEGINNER – BASIC PROGRAMME) #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     float avg;     avg=(a+b)/2.0;     printf("Average of %d and %d is: %0.2f",a,b,avg); }

---
+++ Average_of_two_numbers.c
@@ -0,0 +1,10 @@
+//AVERAGE OF TWO NUMBERS (FIRST LOGIC BEGINNER – BASIC PROGRAMME)
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    float avg;
+    avg=(a+b)/2.0;
+    printf("Average of %d and %d is: %0.2f",a,b,avg);
+}
