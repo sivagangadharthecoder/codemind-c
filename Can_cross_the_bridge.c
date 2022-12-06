c218976de5cf9ff74d07712fb30aac8741f0d297
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2022-12-06 10:43:11 +0530

    //CAN CROSS THE BRIDGE (FIRST LOGIC BEGINNER – BASIC PROGRAMME) #include<stdio.h> int main() {     int x,y,z;     scanf("%d%d%d",&x,&y,&z);     int nm;     nm=(z-y)/x;     printf("%d",nm); }

---
+++ Can_cross_the_bridge.c
@@ -0,0 +1,10 @@
+//CAN CROSS THE BRIDGE (FIRST LOGIC BEGINNER – BASIC PROGRAMME)
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    scanf("%d%d%d",&x,&y,&z);
+    int nm;
+    nm=(z-y)/x;
+    printf("%d",nm);
+}
