a69cd3c4d3efedbe684dc414e8596faf8dcb493e
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2022-12-16 21:19:54 +0530

    #include<stdio.h> int main() {     float a,b,c,d,e,f;     scanf("%f%f%f%f%f",&a,&b,&c,&d,&e);     f=(a+b+c+d+e)/5;     if(f>=90)     {         printf("Grade A");
        } 
        else if(f>=80) 
        {
            printf("Grade B"); 
            
        }
        else if(f>=70) 
        { 
            printf("Grade C");
            }
            else if(f>=60) 
            {
                printf("Grade D"); } else if(f>=40) { printf("Grade E"); } else if(f<40) { printf("Grade F"); }}

---
+++ Grades.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,c,d,e,f; 
+    scanf("%f%f%f%f%f",&a,&b,&c,&d,&e); 
+    f=(a+b+c+d+e)/5; 
+    if(f>=90) 
+    {
+        printf("Grade A"); 
+        
+    } 
+    else if(f>=80) 
+    {
+        printf("Grade B"); 
+        
+    }
+    else if(f>=70) 
+    { 
+        printf("Grade C");
+        }
+        else if(f>=60) 
+        {
+            printf("Grade D"); } else if(f>=40) { printf("Grade E"); } else if(f<40) { printf("Grade F"); }}
