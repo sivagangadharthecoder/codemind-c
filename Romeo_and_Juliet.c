3f4e6ee9170091881a844d7f8e3c3118f684ba3b
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2022-12-16 21:29:17 +0530

    #include<stdio.h> int main(){ int x,y,z; scanf("%d%d%d",&x,&y,&z); int n; n=((x*5)+(y*10))/z; printf("%d",n); }

--- Romeo_and_Juliet.c
+++ Romeo_and_Juliet.c
@@ -1,10 +1,2 @@
 #include<stdio.h>
-int main()
-{
-    int x,y,z; 
-    scanf("%d%d%d",&x,&y,&z); 
-    int n; 
-    n=((x*5)+(y*10))/z; 
-    printf("%d",n);
-    
-}
+int main(){ int x,y,z; scanf("%d%d%d",&x,&y,&z); int n; n=((x*5)+(y*10))/z; printf("%d",n); } 
