db4a094aabc67ca693f808fdbba7a698c86e641f
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2022-12-21 22:31:04 +0530

    #include<stdio.h> int main() {     int in; scanf("%d",&in); float cm; cm= in*2.54; printf("%0.2f",cm);}

--- inches_to_centimeters.c
+++ inches_to_centimeters.c
@@ -1,9 +1,4 @@
 #include<stdio.h>
 int main()
 {
-    int in; 
-    scanf("%d",&in); 
-    float cm; 
-    cm=in*2.54; 
-    printf("%0.2f",cm);
-}
+    int in; scanf("%d",&in); float cm; cm= in*2.54; printf("%0.2f",cm);}
