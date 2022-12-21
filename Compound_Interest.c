59bc3990bcf3737fe8c43de85c0f7d7b1d067d13
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2022-12-21 22:43:03 +0530

    #include<stdio.h> #include<math.h> int main() {     int p,r,t;     scanf("%d%d%d",&p,&r,&t); float ci; ci=p*pow(1+(r/100.0),t)-p; printf("%0.2f",ci);
    }

--- Compound_Interest.c
+++ Compound_Interest.c
@@ -1,14 +1,8 @@
-//COMPOUND INTEREST (FIRST LOGIC BEGINNER – BASIC PROGRAMME)
 #include<stdio.h>
 #include<math.h>
 int main()
 {
-    float p,r,t;
-    scanf("%f%f%f",&p,&r,&t);
-    float ci;
-    ci=p*pow(1+(r/100),t)-p;
-    printf("%0.2f",ci);
+    int p,r,t;
+    scanf("%d%d%d",&p,&r,&t); float ci; ci=p*pow(1+(r/100.0),t)-p; printf("%0.2f",ci);
+    
 }
-
-
-
