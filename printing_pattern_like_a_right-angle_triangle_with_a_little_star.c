b378896e29cfb1a514457e2fae73226c3fc38aa0
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2022-12-25 14:53:52 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=i;j++)         {             printf("*");         }          printf(" ");     }
    }

--- printing_pattern_like_a_right-angle_triangle_with_a_little_star.c
+++ printing_pattern_like_a_right-angle_triangle_with_a_little_star.c
@@ -1,3 +1,16 @@
 #include<stdio.h>
-int main(){int a,i,j;scanf("%d",&a);for(i=1;i<=a;i++){ for(j=0;j<i;j++) { printf("*");}printf("
-");}}
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+         printf("
+");
+    }
+   
+}
