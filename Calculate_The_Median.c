31b46aa0508e3534699f5af44c61eeb6f686c6e0
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2022-12-28 23:37:41 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,j,swap;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);
        }
        for(i=0;i<n;i++)
        { 
            for(j=i+1;j<n;j++)
            {
                if(a[i]>a[j])
                { 
                    swap=a[i];
                    a[i]=a[j]; 
                    a[j]=swap;
                    }
                    }
            
        }
        printf("%d",a[n/2]);
        
    }

--- Calculate_The_Median.c
+++ Calculate_The_Median.c
@@ -2,25 +2,27 @@
 #include<math.h>
 int main()
 {
-	int n,i,j,swap;
-	scanf("%d",&n);
-	int a[n];
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&a[i]);
-	}
-	for(i=0;i<n;i++)
-	{
-    	for(j=i+1;j<n;j++)
-    	{
-    		if(a[i]>a[j])
-    		{
-    			swap=a[i];
-    			a[i]=a[j];
-    			a[j]=swap;
-    		}
-    	 	
-    	}	
-	}
-	printf("%d",a[n/2]);
+    int n,i,j,swap;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        
+    }
+    for(i=0;i<n;i++)
+    { 
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            { 
+                swap=a[i];
+                a[i]=a[j]; 
+                a[j]=swap;
+                }
+                }
+        
+    }
+    printf("%d",a[n/2]);
+    
 }
