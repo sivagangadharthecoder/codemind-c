f2e237ee5b38154eb6e3103587dd87739fc1a9c1
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2022-12-29 22:34:32 +0530

    /* an example program for series */ #include<stdio.h> #include<math.h> int main() {     int n;     scanf("%d",&n);     int a,b,i,k1=0,k2=0;     printf("0 ");     for(i=1;i<n;i++)     {         if(i%2==0)         {             a=pow(2,k1);             printf("%d ",a);             k1++;             }             else{                 b=pow(3,k2);                 printf("%d ",b);                 k2++;
                }
        }
                
    }

---
+++ series.c
@@ -0,0 +1,28 @@
+/* an example program for series */
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n; 
+    scanf("%d",&n); 
+    int a,b,i,k1=0,k2=0; 
+    printf("0 "); 
+    for(i=1;i<n;i++) 
+    {
+        if(i%2==0)
+        {
+            a=pow(2,k1); 
+            printf("%d ",a); 
+            k1++;
+            }
+            else{
+                b=pow(3,k2); 
+                printf("%d ",b); 
+                k2++; 
+                
+            }
+    }
+            
+}
+            
+            
