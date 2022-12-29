c6d9bd680a2de4826112311b5bce491efe0f394b
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2022-12-29 20:49:29 +0530

    #include<stdio.h> int main() { float u,c,b,s; scanf("%f",&u); if(u<=199)c=u*1.20; else if(u>=200&&u<400)c=u*1.50; else if(u>=400&&u<600)c=u*1.80; else if(u>=600)c=u*2.00; if(u>400)c=c+c*0.15; else c=c+100; printf("%0.2f",c);}

--- Current_bill.c
+++ Current_bill.c
@@ -1,4 +1,3 @@
 #include<stdio.h>
 int main()
-{
-    float u,c,b,s; scanf("%f",&u); if(u<=199)c=u*1.20; else if(u>=200&&u<400)c=u*1.50; else if(u>=400&&u<600)c=u*1.80; else if(u>=600)c=u*2.00; if(u>400)c=c+c*0.15; else c=c+100; printf("%0.2f",c);}
+{ float u,c,b,s; scanf("%f",&u); if(u<=199)c=u*1.20; else if(u>=200&&u<400)c=u*1.50; else if(u>=400&&u<600)c=u*1.80; else if(u>=600)c=u*2.00; if(u>400)c=c+c*0.15; else c=c+100; printf("%0.2f",c);}
