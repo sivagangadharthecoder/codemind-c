be2ad5eb3a5c73f16badcb045bfcaf00df847804
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2022-12-29 20:57:35 +0530

    # include<stdio.h> int fib(int);//decleration int main() {     int n,f,b;     scanf("%d",&n);     for(int i=n;;i++)     {         if(fib(i))         {             f=i; break;
            } 
            
        }
        for(int i=n;;i--)
        {
            if(fib(i)) 
            {
                b=i; break;
                }
                }
                int d1=f-n;
                int d2=n-b;
                if(d1>d2) 
                {
                    printf("%d",b);
                    }
                    else if(d2>d1)
                    {
                        printf("%d",f);
                        }
                        else if(d1==d2)
                        {
                            printf("%d %d",b,f); }}int fib(int n){ int a,b,c; if(n==0 || n==1) { printf("%d",n); } else { a=0; b=1; c=a+b; } while(n>c) { a=b; b=c; c=a+b; } if(n==c) { return 1; } else { return 0; }}

--- nearest_fibonacci.c
+++ nearest_fibonacci.c
@@ -1,70 +1,35 @@
 # include<stdio.h>
 int fib(int);//decleration
 int main()
-{ 
-    int n,f,b; 
-    scanf("%d",&n); 
-    for(int i=n;;i++) 
+{
+    int n,f,b;
+    scanf("%d",&n);
+    for(int i=n;;i++)
     {
-        if(fib(i))
+        if(fib(i)) 
         {
             f=i; break; 
             
-        }
-        }
-        for(int i=n;;i--)
+        } 
+        
+    }
+    for(int i=n;;i--)
+    {
+        if(fib(i)) 
         {
-            if(fib(i)) 
-            {
-                b=i; break; 
-                
+            b=i; break;
             }
             }
             int d1=f-n;
-            int d2=n-b; 
+            int d2=n-b;
             if(d1>d2) 
             {
-                printf("%d",b); 
-                
-            } 
-            else if(d2>d1) 
-            {
-                printf("%d",f); 
-                
-            }
-            else if(d1==d2)
-            { 
-                printf("%d %d",b,f); 
-                
-            }
-    
-}
-int fib(int n)
-{
-    int a,b,c;
-    if(n==0 || n==1)
-    {
-        printf("%d",n); 
-        
-    }
-    else 
-    {
-        a=0; b=1; c=a+b;
-        }
-        while(n>c) 
-        {
-            a=b;
-            b=c;
-            c=a+b;
-            } 
-            if(n==c)
-            { 
-                return 1;
+                printf("%d",b);
                 }
-                else 
-                { 
-                    return 0; 
-                    
-                }
-    
-}
+                else if(d2>d1)
+                {
+                    printf("%d",f);
+                    }
+                    else if(d1==d2)
+                    {
+                        printf("%d %d",b,f); }}int fib(int n){ int a,b,c; if(n==0 || n==1) { printf("%d",n); } else { a=0; b=1; c=a+b; } while(n>c) { a=b; b=c; c=a+b; } if(n==c) { return 1; } else { return 0; }}
