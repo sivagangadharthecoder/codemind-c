e6c6ed70ab6795af0418dea2ab64cb19916b4ede
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2022-12-30 17:40:14 +0530

    #include<stdio.h> #include<math.h> int main() {     int n;     scanf("%d",&n);     int i,j,s;     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         for(j=i+1;j<n;j++)         {             if(a[i]>a[j])             {                 s=a[i];                 a[i]=a[j];                 a[j]=s;
                }
            }
            
        }
        printf("%d",a[n/2]);
    }

--- Calculate_The_Median.c
+++ Calculate_The_Median.c
@@ -1,3 +1,28 @@
 #include<stdio.h>
 #include<math.h>
-int main(){ int n,i,j,swap; scanf("%d",&n); int a[n]; for(i=0;i<n;i++) { scanf("%d",&a[i]); } for(i=0;i<n;i++) { for(j=i+1;j<n;j++) { if(a[i]>a[j]) { swap=a[i]; a[i]=a[j]; a[j]=swap; } } } printf("%d",a[n/2]); }
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int i,j,s;
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                s=a[i];
+                a[i]=a[j];
+                a[j]=s;
+                
+            }
+        }
+        
+    }
+    printf("%d",a[n/2]);
+}
