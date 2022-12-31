4cfcd6a1bd99ad44584c8ea4ae82e6fc69abc89e
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2022-12-31 20:08:56 +0530

    #include<stdio.h> int main() {     int a,b,i; scanf("%d%d",&a,&b); for(i=a;i<=b;i++) { if(i%2==0) { printf("%d ",i); } } }

--- Write_a_program_to_get_the_list_of_even_numbers_up_to_a_given_number.c
+++ Write_a_program_to_get_the_list_of_even_numbers_up_to_a_given_number.c
@@ -1,14 +1,4 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,i; 
-    scanf("%d%d",&a,&b);
-    for(i=a;i<=b;i++) 
-    {
-        if(i%2==0) 
-        {
-            printf("%d ",i);
-            }
-            }
-    
-}
+    int a,b,i; scanf("%d%d",&a,&b); for(i=a;i<=b;i++) { if(i%2==0) { printf("%d ",i); } } }
