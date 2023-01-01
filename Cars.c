025a506e48c19f72807253e52e3e16ef157d4610
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2023-01-01 14:54:07 +0530

    #include<stdio.h> int main() {     int a,b; scanf("%d",&a); if(a%4==0) { b=a/4; } else { b=(a/4)+1; } printf("%d",b);}

--- Cars.c
+++ Cars.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
 int main()
 {
-    int a,b; 
-scanf("%d",&a); if(a%4==0) { b=a/4; } else { b=(a/4)+1; } printf("%d",b);}
+    int a,b; scanf("%d",&a); if(a%4==0) { b=a/4; } else { b=(a/4)+1; } printf("%d",b);}
