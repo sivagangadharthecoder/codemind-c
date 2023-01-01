7af6a95b4c03cd6d892b4f151faf11336b4916d6
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2023-01-01 14:42:29 +0530

    #include<stdio.h> int main() {     int n,k,m,a; scanf("%d%d%d",&n,&k,&m); if(n%(k*m)==0) { a=n/(k*m); } else { a=(n/(k*m))+1; } printf("%d",a); }

--- Bags_and_Candies.c
+++ Bags_and_Candies.c
@@ -1,18 +1,4 @@
 #include<stdio.h>
 int main()
 {
-    int n,k,m,a; 
-    scanf("%d%d%d",&n,&k,&m);
-    if(n%(k*m)==0) 
-    {
-        a=n/(k*m); 
-        
-    }
-    else 
-    {
-        a=(n/(k*m))+1; 
-        
-    }
-    printf("%d",a);
-    
-}
+    int n,k,m,a; scanf("%d%d%d",&n,&k,&m); if(n%(k*m)==0) { a=n/(k*m); } else { a=(n/(k*m))+1; } printf("%d",a); }
