e15e96b6b597b17b374dfb578e028ddcacfd05d2
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2023-01-07 19:16:14 +0530

    #include<stdio.h> #include<math.h> int main() {     int n;scanf("%d",&n);int b,i,k1=0,k2=0,a;for(i=0;i<=n;i++){if(i%2==0){ a=2*k1;printf("%d ",a);k1++;}else {printf("%d ",k2);k2++;}}}

--- mixture_of_2_series.c
+++ mixture_of_2_series.c
@@ -1,3 +1,5 @@
 #include<stdio.h>
 #include<math.h>
-int main(){int n;scanf("%d",&n);int b,i,k1=0,k2=0,a;for(i=0;i<=n;i++){if(i%2==0){ a=2*k1;printf("%d ",a);k1++;}else {printf("%d ",k2);k2++;}}}
+int main()
+{
+    int n;scanf("%d",&n);int b,i,k1=0,k2=0,a;for(i=0;i<=n;i++){if(i%2==0){ a=2*k1;printf("%d ",a);k1++;}else {printf("%d ",k2);k2++;}}}
