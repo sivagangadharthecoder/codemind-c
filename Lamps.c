6626f6da2a479c34e7022fa5b1878f79c5f46358
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2023-01-11 21:51:48 +0530

    #include<stdio.h> int main(){ int n,k,x,y,a; scanf("%d%d%d%d",&n,&k,&x,&y); if(n==k) { a=x*n; } else if(n!=k) { if(x>y) { a=(x*k)+(n-k)*y; } else { a=(x*k)+(n-k)*x; } } printf("%d",a);}

--- Lamps.c
+++ Lamps.c
@@ -1,3 +1,2 @@
 #include<stdio.h>
-int main()
-{ int n,k,x,y,a; scanf("%d%d%d%d",&n,&k,&x,&y); if(n==k) { a=x*n; } else if(n!=k) { if(x>y) { a=(x*k)+(n-k)*y; } else { a=(x*k)+(n-k)*x; } } printf("%d",a);}
+int main(){ int n,k,x,y,a; scanf("%d%d%d%d",&n,&k,&x,&y); if(n==k) { a=x*n; } else if(n!=k) { if(x>y) { a=(x*k)+(n-k)*y; } else { a=(x*k)+(n-k)*x; } } printf("%d",a);}
