3ed0c30ae407f30a9a7ac91ed1f471505e837767
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2023-01-12 14:58:13 +0530

    #include<stdio.h> int main(){ int n; scanf("%d",&n); int i,sum=0; int a[n]; for(i=0;i<n;i++) { scanf("%d",&a[i]); } for(i=0;i<n;i++) { if(a[i]%2!=0) { sum=sum+a[i]; } } printf("%d",sum);}

--- ODD_Sum.c
+++ ODD_Sum.c
@@ -1,3 +1,2 @@
 #include<stdio.h>
-int main()
-{ int n; scanf("%d",&n); int i,sum=0; int a[n]; for(i=0;i<n;i++) { scanf("%d",&a[i]); } for(i=0;i<n;i++) { if(a[i]%2!=0) { sum=sum+a[i]; } } printf("%d",sum);}
+int main(){ int n; scanf("%d",&n); int i,sum=0; int a[n]; for(i=0;i<n;i++) { scanf("%d",&a[i]); } for(i=0;i<n;i++) { if(a[i]%2!=0) { sum=sum+a[i]; } } printf("%d",sum);}
