c332e82d29adedde293d13af920bc71ab68000fc
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2023-01-13 21:47:33 +0530

    #include<stdio.h> int main() {     int a,b,c,sum; scanf("%d%d%d",&a,&b,&c); if(a>b&&c>b) { sum=a+c; } else if(b>a&&c>a) { sum=b+c; } else if(a>c&&b>c) { sum=a+b; } printf("%d",sum); }

--- Hermoine_and_Spells.c
+++ Hermoine_and_Spells.c
@@ -1,20 +1,4 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c,sum;
-    scanf("%d%d%d",&a,&b,&c);
-    if(a>b&&c>b)
-    {
-        sum=a+c; 
-        
-    }
-    else if(b>a&&c>a) 
-    {
-        sum=b+c;
-    } else if(a>c&&b>c) 
-    {
-        sum=a+b;
-    }
-    printf("%d",sum); 
-    
-}
+    int a,b,c,sum; scanf("%d%d%d",&a,&b,&c); if(a>b&&c>b) { sum=a+c; } else if(b>a&&c>a) { sum=b+c; } else if(a>c&&b>c) { sum=a+b; } printf("%d",sum); }
