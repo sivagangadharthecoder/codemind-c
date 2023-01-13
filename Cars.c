0f4f283a3153b5f3771e33e461be545efc6c3db8
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2023-01-13 21:38:48 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d",&a);     if(a%4==0)     {         b=a/4;
        }
        else 
        {
            b=(a/4)+1;
        }
            printf("%d",b);
        
    }

--- Cars.c
+++ Cars.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
 int main()
 {
-	int n;
-	scanf("%d",&n);
-	int c;
-	if(n%4==0) c=n/4;
-	else c=(n/4)+1;
-	printf("%d",c);
+    int a,b; 
+    scanf("%d",&a);
+    if(a%4==0) 
+    {
+        b=a/4; 
+        
+    }
+    else 
+    {
+        b=(a/4)+1;
+    }
+        printf("%d",b);
+    
 }
