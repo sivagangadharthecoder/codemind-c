69cd7b0e6fcf3f667613230b5caaed501bf69ba7
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2023-01-14 19:18:44 +0530

    #include<stdio.h> int main() { int i,n; scanf("%d",&n);int fac=1; for(i=1;i<=n;i++) { fac=fac*i; } printf("%d",fac);}

--- factorial_of_a_given_Number.c
+++ factorial_of_a_given_Number.c
@@ -1,12 +1,3 @@
 #include<stdio.h>
 int main()
-{
-    int i,n;
-    scanf("%d",&n);
-int fac=1;
-    for(i=1;i<=n;i++)
-    {
-        fac=fac*i;
-    }
-    printf("%d",fac);
-}
+{ int i,n; scanf("%d",&n);int fac=1; for(i=1;i<=n;i++) { fac=fac*i; } printf("%d",fac);}
