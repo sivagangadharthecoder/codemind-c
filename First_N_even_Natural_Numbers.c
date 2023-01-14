86b2a65429e8c5fc08029e3587e19a7c0dc12873
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2023-01-14 19:16:50 +0530

    #include<stdio.h> int main() {     int a,i;     scanf("%d",&a);     for(i=a*2;i>=1;i=i-2)     {         printf("%d ",i);     } }

--- First_N_even_Natural_Numbers.c
+++ First_N_even_Natural_Numbers.c
@@ -1,2 +1,10 @@
 #include<stdio.h>
-int main(){ int a,i; scanf("%d",&a); for(i=a*2;i>=1;i=i-2) { printf("%d ",i); }}
+int main()
+{
+    int a,i; 
+    scanf("%d",&a); 
+    for(i=a*2;i>=1;i=i-2)
+    {
+        printf("%d ",i); 
+    }
+}
