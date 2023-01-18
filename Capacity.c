ecaaaa85c69be2e063d1b6d77162b0f9cdb0222d
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2023-01-18 20:20:24 +0530

    #include<stdio.h> int main(){ int s,t,b; scanf("%d%d%d",&s,&t,&b); int cap; cap=s*t*b; printf("%d KB",cap);}

--- Capacity.c
+++ Capacity.c
@@ -1,4 +1,2 @@
 #include<stdio.h>
-int main()
-{
-    int s,t,b; scanf("%d%d%d",&s,&t,&b); int cap; cap=s*t*b; printf("%d KB",cap);}
+int main(){ int s,t,b; scanf("%d%d%d",&s,&t,&b); int cap; cap=s*t*b; printf("%d KB",cap);}
