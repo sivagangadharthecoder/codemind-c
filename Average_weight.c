880b9cda2f76c636023246909c9868fc7d5a93e7
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2023-01-21 21:38:55 +0530

    #include<stdio.h> int main() {     int a,b,x; scanf("%d%d%d",&a,&b,&x); int c; c=(3*x)-a-b; printf("%d",c);}

--- Average_weight.c
+++ Average_weight.c
@@ -1,2 +1,4 @@
 #include<stdio.h>
-int main(){ int a,b,x; scanf("%d%d%d",&a,&b,&x); int c; c=(3*x)-a-b; printf("%d",c);}
+int main()
+{
+    int a,b,x; scanf("%d%d%d",&a,&b,&x); int c; c=(3*x)-a-b; printf("%d",c);}
