41009d67bba47a829d5bce3d7f8183029c61ec3b
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2023-01-24 21:12:26 +0530

    #include<stdio.h> int main(){ int x,y,z; scanf("%d%d%d",&x,&y,&z); int nm; nm=(z-y)/x; printf("%d",nm);}

--- Can_cross_the_bridge.c
+++ Can_cross_the_bridge.c
@@ -1,4 +1,2 @@
 #include<stdio.h>
-int main()
-{
-    int x,y,z; scanf("%d%d%d",&x,&y,&z); int nm; nm=(z-y)/x; printf("%d",nm);}
+int main(){ int x,y,z; scanf("%d%d%d",&x,&y,&z); int nm; nm=(z-y)/x; printf("%d",nm);}
