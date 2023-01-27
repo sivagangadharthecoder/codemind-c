5859ddd97ec2f871f53e7633ab04d6c0bc471e36
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2023-01-27 21:41:18 +0530

    #include<stdio.h> int main(){ int a,b; scanf("%d%d",&a,&b); if(a+b>=11) { printf("%d",21-a-b); } else { printf("-1"); }}

--- Blackjack.c
+++ Blackjack.c
@@ -1,16 +1,2 @@
 #include<stdio.h>
-int main()
-{
-    int a,b;
-    scanf("%d%d",&a,&b);
-    if(a+b>=11)
-    {
-        printf("%d",21-a-b);
-    }
-        else 
-        {
-            printf("-1"); 
-            
-        }
-    
-}
+int main(){ int a,b; scanf("%d%d",&a,&b); if(a+b>=11) { printf("%d",21-a-b); } else { printf("-1"); }}
