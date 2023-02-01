acad76f0791b88f3539cd44e180784671b68836d
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2023-02-01 20:31:00 +0530

    #include<stdio.h> int main() { int a,b,c,d; scanf("%d%d%d%d",&a,&b,&c,&d); if(a>=10 && b>=10 && c>=10 && d>=10) { printf("YES"); } else{ printf("NO"); }}

--- Target.c
+++ Target.c
@@ -1,4 +1,3 @@
 #include<stdio.h>
 int main()
-{
-    int a,b,c,d; scanf("%d%d%d%d",&a,&b,&c,&d); if(a>=10 && b>=10 && c>=10 && d>=10) { printf("YES"); } else{ printf("NO"); }}
+{ int a,b,c,d; scanf("%d%d%d%d",&a,&b,&c,&d); if(a>=10 && b>=10 && c>=10 && d>=10) { printf("YES"); } else{ printf("NO"); }}
