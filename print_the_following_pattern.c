f320afaa230039ffc2ce87800e065c1b3819be76
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2023-05-20 10:06:39 +0530

    #include<stdio.h> int main() { 	int i,j,n,t; 	scanf("%d",&n); 	for(i=n,t=(64+n);i>=1;i--,t--) 	{ 		for(j=1;j<=i;j++) 		{ 			printf("%c ",t); 	    } 		printf(" "); 	} }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,11 +3,11 @@ int main()
 {
 	int i,j,n,t;
 	scanf("%d",&n);
-	for(i=1;i<=n;i++)
+	for(i=n,t=(64+n);i>=1;i--,t--)
 	{
 		for(j=1;j<=i;j++)
 		{
-			printf("%d",j);
+			printf("%c ",t);
 	    }
 		printf("
 ");
