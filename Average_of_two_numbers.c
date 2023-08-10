5ee9ab0fdf6627e5c260615e0b08e9931f18dac4
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2023-08-10 21:29:47 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     float avg= (a+b)/2.0;
        printf("Average of %d and %d is: %0.2f",a,b,avg);
    }

--- Average_of_two_numbers.c
+++ Average_of_two_numbers.c
@@ -3,9 +3,7 @@ int main()
 {
     int a,b;
     scanf("%d%d",&a,&b);
-    float avg;
-    {
-        avg=(a+b)/2.0;
-        printf("Average of %d and %d is: %0.2f",a,b,avg);
-    }
+    float avg= (a+b)/2.0;
+    
+    printf("Average of %d and %d is: %0.2f",a,b,avg);
 }
