448ca82b6e069c682f49bfabf8e48e2194db6f9d
sivagangadharthecoder <119839967+sivagangadharthecoder@users.noreply.github.com>
2023-08-22 23:42:13 +0530

    #include<stdio.h> int main() {     int i,j,n,temp;     scanf("%d",&n);     int array[n];     for(i=0;i<n;i++)     {         scanf("%d",&array[i]);     }
        for(i=1; i<n; i++)
        {
            for(j=0;j<n-1;j++)
            {
                if(array[j]>array[j+1])
                {
                    temp = array[j];
                    array[j] = array[j+1];
                    array[j+1] = temp;
                }
            }
        }
        for(i=0;i<n;i++)
        {
            printf("%d ",array[i]);
        }
        
        return 0;
    }

--- Sort_an_Array_of_0s_and_1s.c
+++ Sort_an_Array_of_0s_and_1s.c
@@ -1,37 +1,31 @@
 #include<stdio.h>
-
-void bubble_sort_function(int *array,int size)
-{
-	int i,j;
-	for(i=0; i<size-1; i++)
-	{
-		for(j=0; j<size-1; j++)
-		{
-			if (array[j] > array[j+1])
-			{
-				int temp = array[j];
-				array[j] = array[j+1];
-				array[j+1] = temp;
-			}
-		}
-	}
-}
-
 int main()
 {
-	int size;
-	scanf("%d",&size);
-	int array[size],i;
-	for (i=0; i<size; i++)
-	{
-		scanf("%d",&array[i]);
-	}
-	
-	bubble_sort_function(array, size);
-	
-	for (i=0; i<size; i++)
-	{
-		printf("%d ",array[i]);
-	}
-	return 0;
+    int i,j,n,temp;
+    scanf("%d",&n);
+    int array[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&array[i]);
+    }
+    
+  
+    for(i=1; i<n; i++)
+    {
+        for(j=0;j<n-1;j++)
+        {
+            if(array[j]>array[j+1])
+            {
+                temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",array[i]);
+    }
+    
+    return 0;
 }
